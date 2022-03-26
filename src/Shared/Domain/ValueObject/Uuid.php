73dc92737e9e421ac947d175b84d3cec7b37b305
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-03-26 22:33:28 +0900

    create shared domain value object uuid class

---
+++ src/Shared/Domain/ValueObject/Uuid.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Shared\Domain\ValueObject;
+
+use InvalidArgumentException;
+use Stringable;
+use Ramsey\Uuid\Uuid as RamseyUuid;
+
+class Uuid implements Stringable
+{
+    protected string $value;
+
+    public function __construct(string $value)
+    {
+        $this->ensureIsValidUuid($value);
+        $this->value = $value;
+    }
+
+    public static function generate(): self
+    {
+        return new static(RamseyUuid::uuid4()->toString());
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    public function equals(Uuid $other): bool
+    {
+        return $this->value() === $other->value();
+    }
+
+    public function __toString(): string
+    {
+        return $this->value();
+    }
+
+    private function ensureIsValidUuid(string $id): void
+    {
+        if (!RamseyUuid::isValid($id)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    '<%s> does not allow the value <%s>.',
+                    static::class,
+                    $id
+                )
+            );
+        }
+    }
+}
