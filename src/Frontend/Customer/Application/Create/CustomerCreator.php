6d76b2d138db20fde9b63942c91bb43474cd9baa
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-03-27 12:50:50 +0900

    Update CustomerCreator.php

--- src/Frontend/Customer/Application/Create/CustomerCreator.php
+++ src/Frontend/Customer/Application/Create/CustomerCreator.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace Src\Frontend\Customer\Application\Create;
 
+use Src\Frontend\Customer\Domain\Customer;
+use Src\Frontend\Customer\Domain\CustomerEmail;
+use Src\Frontend\Customer\Domain\CustomerFirstName;
+use Src\Frontend\Customer\Domain\CustomerLastName;
+use Src\Frontend\Customer\Domain\CustomerPassword;
 use Src\Frontend\Customer\Domain\CustomerRepository;
+use Src\Frontend\Customer\Domain\CustomerUuid;
 
 final class CustomerCreator
 {
@@ -15,7 +21,17 @@ final class CustomerCreator
         $this->customerRepository = $customerRepository;
     }
 
-    public function __invoke(): void
+    public function __invoke(array $attributes): void
     {
+        $customer = Customer::create(
+            CustomerUuid::generate(),
+            new CustomerEmail($attributes['email']),
+            new CustomerPassword($attributes['password']),
+            new CustomerFirstName($attributes['first_name']),
+            new CustomerLastName($attributes['last_name']),
+        );
+
+        $this->customerRepository
+            ->save($customer);
     }
 }
