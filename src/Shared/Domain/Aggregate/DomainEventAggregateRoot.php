c2ed5304a5a4dd50857c5fa4d9d246fb2d9456f1
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-03-27 16:12:59 +0900

    Create DomainEventAggregateRoot.php

---
+++ src/Shared/Domain/Aggregate/DomainEventAggregateRoot.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Src\Shared\Domain\Aggregate;
+
+use Src\Shared\Domain\Bus\Event\DomainEvent;
+
+abstract class DomainEventAggregateRoot
+{
+    private array $domainEvents = [];
+
+    final public function pullDomainEvents(): array
+    {
+        $domainEvents       = $this->domainEvents;
+        $this->domainEvents = [];
+
+        return $domainEvents;
+    }
+
+    final protected function record(DomainEvent $domainEvent): void
+    {
+        $this->domainEvents[] = $domainEvent;
+    }
+}
