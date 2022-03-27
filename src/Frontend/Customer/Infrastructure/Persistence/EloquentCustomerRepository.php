390417fcf50a69bafb2c203cd9b5c1425b002392
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-03-27 12:48:57 +0900

    move to persistence directory

--- src/Frontend/Customer/Infrastructure/Persistence/EloquentCustomerRepository.php
+++ src/Frontend/Customer/Infrastructure/Persistence/EloquentCustomerRepository.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Src\Frontend\Customer\Infrastructure;
+namespace Src\Frontend\Customer\Infrastructure\Persistence;
 
-use Src\Frontend\Customer\Domain\Customer;
 use App\Models\Customer as EloquentModelCustomer;
+use Src\Frontend\Customer\Domain\Customer;
 use Src\Frontend\Customer\Domain\CustomerRepository;
 use Src\Frontend\Customer\Domain\CustomerUuid;
 
@@ -13,7 +13,12 @@ final class EloquentCustomerRepository implements CustomerRepository
 {
     public function save(Customer $customer): void
     {
-        EloquentModelCustomer::create($customer->toPrimitives());
+        EloquentModelCustomer::updateOrCreate(
+            [
+                'uuid' => $customer->uuid()->value()
+            ],
+            $customer->toPrimitives()
+        );
     }
 
     public function find(CustomerUuid $uuid): ?Customer
