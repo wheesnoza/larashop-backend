694d0fd56f4c9b841904a356eada923e1d54f457
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-03-27 12:49:15 +0900

    register customer repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Src\Frontend\Customer\Domain\CustomerRepository;
+use Src\Frontend\Customer\Infrastructure\Persistence\EloquentCustomerRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            CustomerRepository::class,
+            EloquentCustomerRepository::class
+        );
     }
 
     /**
