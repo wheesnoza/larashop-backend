c9309c9ede55f0f8abe7a67ebf7f5d01c15593ea
wheesnoza <45123151+wheesnoza@users.noreply.github.com>
2022-04-17 15:14:33 +0900

    make format

--- app/Shared/Providers/AppServiceProvider.php
+++ app/Shared/Providers/AppServiceProvider.php
@@ -5,8 +5,6 @@ namespace App\Shared\Providers;
 use Illuminate\Support\ServiceProvider;
 use Src\Frontend\Customer\Domain\CustomerRepository;
 use Src\Frontend\Customer\Infrastructure\Persistence\EloquentCustomerRepository;
-use Src\Frontend\Product\Domain\ProductRepository;
-use Src\Frontend\Product\Infrastructure\Persistence\EloquentProductRepository;
 use Src\Shared\Domain\Bus\Event\EventBus;
 use Src\Shared\Infrastructure\Bus\Event\LaravelEventBus;
 
